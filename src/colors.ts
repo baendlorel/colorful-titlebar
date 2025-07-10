3e43d5d7a78fdb30c04520cbb26d870052618bf5
aldia_mechdream <futami16237@gmail.com>
2025-07-10 20:24:46 +0800

    feat: 优化RGBColor类的处理，增加了可以变灰变暗的函数； 现在获取颜色套组和插值都只会返回RGBColor对象； 增加ColorSet类型，就是颜色套组类型； getBrightness函数改为isDarkTheme，专门用于传值进入colors.ts来判定用哪个颜色套组

--- src/colors.ts
+++ src/colors.ts
@@ -1,25 +1,20 @@
-class RGBColor {
-  r: number;
-  g: number;
-  b: number;
+export class RGBColor {
+  r: number = 0;
+  g: number = 0;
+  b: number = 0;
+  a: number = 1;
 
   constructor(s?: string) {
     if (!s) {
-      this.r = 0;
-      this.g = 0;
-      this.b = 0;
       return;
     }
-
+    s = s.replace(/\s/g, '');
     const matched = s.match(/\d+/g);
-    if (!matched || matched.length !== 3) {
-      this.r = 0;
-      this.g = 0;
-      this.b = 0;
+    if (!matched || (matched.length !== 3 && matched.length !== 4)) {
       console.error(
         'Invalid RGB color string:',
         s,
-        'expected format "rgb(r, g, b)". Now return black'
+        'expected format "#000000, #00000000, rgb(r, g, b), rgba(r, g, b, a), hsl, hsla". Now return black'
       );
       return;
     }
@@ -29,38 +24,68 @@ class RGBColor {
     this.b = parseInt(matched[2], 10);
   }
 
-  toHexColor() {
-    const r = Math.round(this.r).toString(16);
-    const g = Math.round(this.g).toString(16);
-    const b = Math.round(this.b).toString(16);
+  toString() {
+    const r = Math.round(this.r).toString(16).padStart(2, '0');
+    const g = Math.round(this.g).toString(16).padStart(2, '0');
+    const b = Math.round(this.b).toString(16).padStart(2, '0');
+    return `#${r}${g}${b}`;
+  }
+
+  toGreyDarkenString(intensity = 0.2) {
+    // intensity 越大，越灰且越暗，建议 0.1 ~ 0.5
+    const gray = (this.r + this.g + this.b) / 3;
+
+    const mix = (channel: number) => {
+      const darker = channel * (1 - intensity);
+      const grayer = gray * intensity;
+      return Math.round(darker + grayer);
+    };
+
+    const r = Math.round(mix(this.r)).toString(16).padStart(2, '0');
+    const g = Math.round(mix(this.g)).toString(16).padStart(2, '0');
+    const b = Math.round(mix(this.b)).toString(16).padStart(2, '0');
     return `#${r}${g}${b}`;
   }
 }
 
-const colorTableLight = [
-  'rgb(147, 197, 253)',
-  'rgb(128, 203, 196)',
-  'rgb(172, 243, 157)',
-  'rgb(250, 204, 21)',
-  'rgb(253, 151, 31)',
-  'rgb(251, 113, 133)',
-  'rgb(167, 139, 250)',
-  'rgb(147, 197, 253)', // Same as the first color
-];
+interface ColorSet {
+  dark: string[];
+  light: string[];
+}
 
-const colorTableDark = [
-  'rgb(10, 78, 135)',
-  'rgb(0, 100, 100)',
-  'rgb(12, 118, 51)',
-  'rgb(135, 107, 0)',
-  'rgb(135, 87, 36)',
-  'rgb(135, 0, 0)',
-  'rgb(75, 0, 130)',
-  'rgb(10, 78, 135)', // Same as the first color
-];
+const defaultColorSet: ColorSet = {
+  light: [
+    'rgb(147, 197, 253)',
+    'rgb(128, 203, 196)',
+    'rgb(172, 243, 157)',
+    'rgb(250, 204, 21)',
+    'rgb(253, 151, 31)',
+    'rgb(251, 113, 133)',
+    'rgb(167, 139, 250)',
+    'rgb(147, 197, 253)', // Same as the first color
+  ],
+  dark: [
+    'rgb(10, 78, 135)',
+    'rgb(0, 100, 100)',
+    'rgb(12, 118, 51)',
+    'rgb(135, 107, 0)',
+    'rgb(135, 87, 36)',
+    'rgb(135, 0, 0)',
+    'rgb(75, 0, 130)',
+    'rgb(10, 78, 135)', // Same as the first color
+  ],
+};
 
-export const getColor = (k: number, isDark: boolean): string => {
-  const table = isDark ? colorTableDark : colorTableLight;
+/**
+ * 获取颜色套组
+ * @param k 0~1之间的比例值
+ * @param isDark 是否为暗色主题
+ * @param colorSet 颜色套组可能从config中获取，没有则使用默认套组
+ * @returns
+ */
+export const getColorSet = (k: number, isDark: boolean, colorSet?: ColorSet): RGBColor => {
+  colorSet = colorSet ?? defaultColorSet;
+  const table = isDark ? colorSet.dark : colorSet.light;
   const n = table.length;
   const nto1 = 1 / n;
   const a = Math.floor(k * n);
@@ -72,11 +97,11 @@ export const getColor = (k: number, isDark: boolean): string => {
   return interpolate(c1, c2, factor);
 };
 
-function interpolate(c1: RGBColor, c2: RGBColor, factor: number): string {
+function interpolate(c1: RGBColor, c2: RGBColor, factor: number): RGBColor {
   const c = new RGBColor();
   c.r = Math.floor(c1.r + (c2.r - c1.r) * factor);
   c.g = Math.floor(c1.g + (c2.g - c1.g) * factor);
   c.b = Math.floor(c1.b + (c2.b - c1.b) * factor);
 
-  return c.toHexColor();
+  return c;
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,67 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
-import { generateColor } from './utils';
+import { basename, join } from 'node:path';
+import { readFile } from 'node:fs';
 
-export function activate(context: vscode.ExtensionContext) {
+import { generateColor } from './utils';
+import { RGBColor } from './colors';
+
+export function activate(_context: vscode.ExtensionContext) {
   const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
 
-  let color = '';
+  let color = new RGBColor('#007ACC');
   if (workspaceFolder) {
     const folderPath = workspaceFolder.uri.fsPath;
-    const projectName = path.basename(folderPath);
+    const projectName = basename(folderPath);
     vscode.window.showInformationMessage(`projectName:${projectName}`);
-    color = generateColor(projectName, getBrightness());
+    color = generateColor(projectName, isDarkTheme());
   }
 
   vscode.window.showInformationMessage(`process.execPath: ${process.execPath}`);
 
   // 将状态栏项添加到订阅中，确保在扩展停用时清理
-  vscode.window.showInformationMessage(`插件已激活！颜色: ${color}`);
+  vscode.window.showInformationMessage(`插件已激活！颜色: ${color.toString()}`);
 
   const config = vscode.workspace.getConfiguration();
   config.update(
     'workbench.colorCustomizations',
     {
-      'titleBar.activeBackground': color,
-      'titleBar.inactiveBackground': color,
+      'titleBar.activeBackground': color.toString(),
+      'titleBar.inactiveBackground': color.toGreyDarkenString(),
     },
     null
     // vscode.ConfigurationTarget.Global
   );
+  purgeSettingsFile();
 }
 
-function getBrightness() {
+function isDarkTheme(): boolean {
   switch (vscode.window.activeColorTheme.kind) {
     case vscode.ColorThemeKind.Dark:
     case vscode.ColorThemeKind.HighContrast:
-      return { max: 40, min: 25 }; // 暗色主题: 25-40% (深色)
+      return true;
     case vscode.ColorThemeKind.Light:
     case vscode.ColorThemeKind.HighContrastLight:
-      return { max: 100, min: 70 }; // 暗色主题: 25-40% (深色)
+      return false;
+  }
+}
+
+function purgeSettingsFile() {
+  const folders = vscode.workspace.workspaceFolders;
+  if (folders && folders.length > 0) {
+    vscode.window.showInformationMessage(folders.map((f) => f.name).join(', '));
+    const workspacePath = folders[0].uri.fsPath;
+    const filePath = join(workspacePath, 'settings.json');
+    readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        vscode.window.showErrorMessage(`读取失败: ${err.message}`);
+      } else {
+        vscode.window.showInformationMessage(`文件内容是: ${data}`);
+      }
+    });
+  } else {
+    vscode.window.showInformationMessage(
+      `has folders? ${Array.isArray(folders)}, folders?.length: ${folders?.length}`
+    );
   }
 }
 
