fb190254f38dc905419757438e531deb3e45b7a0
aldia_mechdream <futami16237@gmail.com>
2025-07-10 14:34:26 +0800

    feat: 已经可以做到每个项目名字标题栏颜色不一样

--- src/extension.ts
+++ src/extension.ts
@@ -4,21 +4,19 @@ import * as crypto from 'crypto';
 
 export function activate(context: vscode.ExtensionContext) {
   const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-  vscode.window.showInformationMessage(`workspaceFolder:${workspaceFolder}`);
 
-  let color: string;
-  // set default color
+  let color = '';
   if (workspaceFolder) {
     const folderPath = workspaceFolder.uri.fsPath;
     const projectName = path.basename(folderPath);
     vscode.window.showInformationMessage(`projectName:${projectName}`);
-    color = generateColorFromName(projectName);
-    return;
-  } else {
-    color = '#FF2222';
+    color = generateColor(projectName);
   }
 
   vscode.window.showInformationMessage(`color:${color}`);
+
+  // 将状态栏项添加到订阅中，确保在扩展停用时清理
+  vscode.window.showInformationMessage(`插件已激活！颜色: ${color}`);
 
   const config = vscode.workspace.getConfiguration();
   config.update(
@@ -26,21 +24,36 @@ export function activate(context: vscode.ExtensionContext) {
     {
       'titleBar.activeBackground': color,
       'titleBar.inactiveBackground': color,
-      'titleBar.activeForeground': '#ffffff',
-      'titleBar.inactiveForeground': '#dddddd',
     },
-    vscode.ConfigurationTarget.Global
+    null
+    // vscode.ConfigurationTarget.Global
   );
 }
 
-function generateColorFromName(name: string): string {
-  // 创建稳定哈希（取前两个字节）
-  const hash = crypto.createHash('md5').update(name).digest();
-  const h = hash[0] % 360; // hue: 0 ~ 359
-  const s = 60 + (hash[1] % 30); // saturation: 60% ~ 89%
-  const l = 50; // lightness: 固定亮度
+function generateColor(fileName: string): string {
+  const hash = crypto.createHash('md5').update(fileName).digest();
 
-  return `hsl(${h}, ${s}%, ${l}%)`;
+  const brightness = getBrightness();
+
+  // 将0-255映射到指定的亮度范围
+  const range = brightness.max - brightness.min;
+  const color = (i: number) => Math.floor(brightness.min + (hash[i] * range) / 255).toString(16);
+  const r = color(0);
+  const g = color(1);
+  const b = color(2);
+
+  return `#${r}${g}${b}`;
+}
+
+function getBrightness() {
+  switch (vscode.window.activeColorTheme.kind) {
+    case vscode.ColorThemeKind.Dark:
+    case vscode.ColorThemeKind.HighContrast:
+      return { max: 40, min: 25 }; // 暗色主题: 25-40% (深色)
+    case vscode.ColorThemeKind.Light:
+    case vscode.ColorThemeKind.HighContrastLight:
+      return { max: 100, min: 70 }; // 暗色主题: 25-40% (深色)
+  }
 }
 
 export function deactivate() {}
