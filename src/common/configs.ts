3d1188d54bf45d30319711dbb04041865261872e
aldia_mechdream <futami16237@gmail.com>
2025-07-14 12:03:45 +0800

    fix: 删除同值enum，不太合适

--- src/common/configs.ts
+++ src/common/configs.ts
@@ -27,8 +27,6 @@ export const enum ConfigSection {
 }
 
 const enum Defaults {
-  ShowInfoPop = 1,
-  ShowSuggest = 1,
   LightThemeColors = 'rgb(167, 139, 250);rgb(147, 197, 253);rgb(128, 203, 196);rgb(172, 243, 157);rgb(250, 204, 21);rgb(253, 151, 31);rgb(251, 113, 133)',
   DarkThemeColors = 'rgb(68, 0, 116);rgb(0, 47, 85);rgb(0, 66, 66);rgb(0, 75, 28);rgb(124, 65, 1);rgb(133, 33, 33)',
   ProjectIndicators = '.git;package.json;pom.xml;Cargo.toml;go.mod;README.md;LICENSE;tsconfig.json;yarn.lock;pnpm-lock.yaml;package-lock.json;webpack.config.js;vite.config.js;vite.config.ts;next.config.js;pyproject.toml;setup.py;CMakeLists.txt;Makefile;build.gradle;composer.json;Gemfile;.nvmrc;.node-version;Deno.json;deno.jsonc',
